2dafafddf5e3c3a5bc584d10044ee77cfa2d7627
Matt Seidholz <mseidholz@gmail.com>
2023-04-27 12:15:58 -0400

    add rose entity, migrate rose to table, add auto-generated uuids

---
+++ db/migrate/20230427160608_create_roses.rb
@@ -0,0 +1,21 @@
+class CreateRoses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :roses do |t|
+      t.uuid :private_id
+      t.text :name
+      t.uuid :species_id
+      t.uuid :variety_id
+      t.text :primary_color
+      t.text :accent_color
+      t.float :location_x
+      t.float :location_y
+      t.date :acquired_date
+      t.boolean :is_deleted
+
+      t.timestamps
+    end
+    add_index :roses, :name, unique: true
+    add_index :roses, :species_id
+    add_index :roses, :variety_id
+  end
+end
---
+++ db/migrate/20230427161524_enable_pgcrypto_extension.rb
@@ -0,0 +1,4 @@
+class EnablePgcryptoExtension < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
