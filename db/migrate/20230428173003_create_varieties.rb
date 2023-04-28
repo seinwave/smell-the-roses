065f39d6a09c1d8c722d556463f41a5385446e48
Matt Seidholz <mseidholz@gmail.com>
2023-04-28 13:34:56 -0400

    migrate variety, category models

--- db/migrate/20230428173003_create_varieties.rb
+++ db/migrate/20230428173003_create_varieties.rb
@@ -1,7 +1,7 @@
 class CreateVarieties < ActiveRecord::Migration[7.0]
   def change
     create_table :varieties do |t|
-      t.string :name unique: true null: false
+      t.string :name, unique: true, null: false
       t.string :description
 
       t.timestamps
