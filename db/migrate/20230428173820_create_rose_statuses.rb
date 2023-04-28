b295a480575b1cd82de8b915055a4288d910f890
Matt Seidholz <mseidholz@gmail.com>
2023-04-28 13:41:21 -0400

    add status

---
+++ db/migrate/20230428173820_create_rose_statuses.rb
@@ -0,0 +1,9 @@
+class CreateRoseStatuses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :rose_statuses do |t|
+      t.timestamps
+      t.references :rose, null: false, foreign_key: true
+      t.integer :status, null: false, default: 0
+    end
+  end
+end
