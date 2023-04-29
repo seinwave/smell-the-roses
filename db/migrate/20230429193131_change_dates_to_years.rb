e236289b08fed8cba5c9202e29925a39f06fa660
Matt Seidholz <mseidholz@gmail.com>
2023-04-29 15:38:45 -0400

    try totally replacing columns

--- db/migrate/20230429193131_change_dates_to_years.rb
+++ db/migrate/20230429193131_change_dates_to_years.rb
@@ -1,8 +1,8 @@
 class ChangeDatesToYears < ActiveRecord::Migration[7.0]
   def change
-    change_column :breeds, :bred_date, :integer
-    change_column :breeds, :introduced_date, :integer
-    rename_column :breeds, :bred_date, :bred_year
-    rename_column :breeds, :introduced_date, :introduced_year
+    remove_column :breeds, :bred_date, :date
+    remove_column :breeds, :introduced_date, :date
+    add_column :breeds, :bred_year, :integer
+    add_column :breeds, :introduced_year, :integer
   end
 end
