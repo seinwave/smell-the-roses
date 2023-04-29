8c274493773466e2d20ab4f95469f2decee6a219
Matt Seidholz <mseidholz@gmail.com>
2023-04-29 11:58:16 -0400

    adjust migration

--- db/migrate/20230429155530_associate_category_with_breed.rb
+++ db/migrate/20230429155530_associate_category_with_breed.rb
@@ -1,5 +1,8 @@
 class AssociateCategoryWithBreed < ActiveRecord::Migration[7.0]
   def change
     add_reference :breeds, :category, null: false, foreign_key: true
+    add_column :breeds, :description, :string
+    add_column :breeds, is_deleted, :boolean, default: false, null: false
+    add_column :breeds, :category_id, :bigint, null: false
   end
 end
