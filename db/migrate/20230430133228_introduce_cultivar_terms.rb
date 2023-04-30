dcf634318d4b9b1b258aa3708e56cd2ef0239fcd
Matt Seidholz <mseidholz@gmail.com>
2023-04-30 09:41:30 -0400

    execute migration

--- db/migrate/20230430133228_introduce_cultivar_terms.rb
+++ db/migrate/20230430133228_introduce_cultivar_terms.rb
@@ -2,8 +2,8 @@ class IntroduceCultivarTerms < ActiveRecord::Migration[7.0]
   def change
     rename_table :breeds, :cultivars
     rename_table :varieties, :cultivar_groups
-    delete_table :species
-    delete_column :cultivars, :species_id
+    remove_column :cultivars, :species_id
+    drop_table :species
     add_column :cultivars, :cultivar_group_id, :int, null: false
     add_column :cultivar_groups, :category_id, :int, null: false
   end
