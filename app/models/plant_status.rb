f49e2a1a4b75014df568bbc2e2206dfa776565ab
Matt Seidholz <mseidholz@gmail.com>
2023-05-20 09:35:27 -0400

    add status view

--- app/models/plant_status.rb
+++ app/models/plant_status.rb
@@ -2,7 +2,4 @@ class PlantStatus < ApplicationRecord
       enum :status, { peak: 0, budding:1, blooming: 2, dormant:3 }
       belongs_to :plant
 
-      # def status_word
-      #   PlantStatus.statuses.key(status_type).to_s
-      # end 
 end
