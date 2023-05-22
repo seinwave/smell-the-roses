77a14313322960da75929dc352458eb121d9adbe
Matt Seidholz <mseidholz@gmail.com>
2023-05-22 10:35:15 -0400

    add factory invocations to test

--- spec/models/plant_status_spec.rb
+++ spec/models/plant_status_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PlantStatus, type: :model do
+  before(:all) do 
+    @plant = create(:plant)
+  end
   it 'should not save a status without a status entry' do
     status = PlantStatus.new
     expect(status.save).to eq(false)
@@ -15,7 +18,7 @@ RSpec.describe PlantStatus, type: :model do
   it 'should save, with a status and plant_id' do
     status = PlantStatus.new
     status.status = 0
-    status.plant_id = 1
+    status.plant_id = @plant.id
     expect(status.save).to eq(true)
   end
 end
