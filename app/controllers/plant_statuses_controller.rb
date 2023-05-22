4b984bba26b3e184488eb32de8e70e55e5042787
Matt Seidholz <mseidholz@gmail.com>
2023-05-22 13:54:32 -0400

    redlight test on failed creation

--- app/controllers/plant_statuses_controller.rb
+++ app/controllers/plant_statuses_controller.rb
@@ -6,5 +6,23 @@ class PlantStatusesController < ApplicationController
   def show
       @plant_status = PlantStatus.find(params[:id])
   end
-  
+
+  def new
+    @article = Article.new
+  end 
+
+  def create
+      @plant_status = PlantStatus.new(plant_status_params)
+      if @plant_status.save
+        redirect_to @plant_status
+      else 
+        render @plant_status.errors, status: :unprocessable_entity
+      end 
+  end
+
+  private
+  def plant_status_params
+      params.require(:plant_status).permit(:status, :plant_id)
+  end
+
 end
--- spec/requests/plant_status_spec.rb
+++ spec/requests/plant_status_spec.rb
@@ -1,15 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe "PlantStatuses", type: :request do
-  it 'should show a specific status' do 
-    status = PlantStatus.new
-    status.status = 0
-    status.plant_id = 1
-    status.save
-    expect(status.save).to eq(true)
-    url = "URL TO VISIT", "/plant_statuses/#{status.id}"
-    get "/plant_statuses/#{status.id}"
-    expect(response).to have_http_status(:success)
-    expect(response.body).to include("peak")
+
+  
+  describe "GET /plant_statuses" do
+    it 'should show all statuses' do
+      get "/plant_statuses"
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include("PLANT STATUSES")
+    end
+    
+    it 'should show a specific status' do 
+      status = PlantStatus.new
+      status.status = 0
+      status.plant_id = 1
+      status.save
+      expect(status.save).to eq(true)
+      get "/plant_statuses/#{status.id}"
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include("peak")
+    end
+  end 
+
+  describe "POST /plant_statuses" do
+    it 'should create a new status, with valid parameters' do
+    parameters = { plant_status:{status: 'peak', plant_id: 1} }
+    post "/plant_statuses", params: parameters
+    expect(response).to have_http_status(:redirect)
+    expect(PlantStatus.count).to eq(1)
+    expect(PlantStatus.last.status).to eq("peak")
   end
+
+    it 'should redirect to an error page, with invalid parameters' do
+    parameters = { status: 'peak' }
+    post "/plant_statuses", params: parameters
+    puts response 
+    expect(response).to have_http_status(:failure)
+   
+  end
+end
+
+  # it 'should update a selected status' do
+  #   parameters = { status_id: 1, status: 'budding' }
+  #   post "/plant_statuses", params: parameters
+  #   expect(response).to have_http_status(:redirect)
+  #   expect(PlantStatus.count).to eq(1)
+  #   expect(PlantStatus.last.status).to eq("peak")
+  # end
+
+
 end
