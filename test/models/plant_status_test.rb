b1d77cf1dcc10358c23ab5b2feeab6a8dc06d5c1
Matt Seidholz <mseidholz@gmail.com>
2023-05-22 09:08:26 -0400

    add first status unit test

--- test/models/plant_status_test.rb
+++ test/models/plant_status_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a status without a status entry" do
+    status = Status.new
+    assert_not status.save
+  end
 end
