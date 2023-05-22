10fec05e27c6941f278ebd3650059f704bbc8650
Matt Seidholz <mseidholz@gmail.com>
2023-05-22 09:56:37 -0400

    add second unit test to plant_status

--- spec/models/plant_status_spec.rb
+++ spec/models/plant_status_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe PlantStatus, type: :model do
     status = PlantStatus.new
     expect(status.save).to eq(false)
   end
+
+  it 'should not save a status without a plant_id' do
+    status = PlantStatus.new
+    status.status = 0
+    expect(status.save).to eq(false)
+  end
 end
