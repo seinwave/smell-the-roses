58d7ad5a10d5210046bb375b80afb901d7fe433f
Matt Seidholz <mseidholz@gmail.com>
2023-06-02 13:47:47 -0400

    change location figures to floats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_173928) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_174701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,8 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_173928) do
   end
 
   create_table "plants", force: :cascade do |t|
-    t.integer "location_x"
-    t.integer "location_y"
+    t.float "location_x"
+    t.float "location_y"
     t.boolean "is_deleted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
