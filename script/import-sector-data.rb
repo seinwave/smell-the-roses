20b1177fd68271631b972925ab16eac5a457579f
Matt Seidholz <mseidholz@gmail.com>
2023-06-19 09:42:04 -0400

    created plant-adding script -- haven't run it yet

--- script/import-sector-data.rb
+++ script/import-sector-data.rb
@@ -10,14 +10,20 @@ require 'json'
 dir_name = "script/sectors"
 
 
+
 Dir.children(dir_name).each do |file_name|
+    failed = []
+    cultivar_names_to_add = []
+
+    # FIRST: create the sector, if it doesn't exist
     data = JSON.parse(File.read(File.join(dir_name, file_name)))
     sector = data['features'].select {|feature| feature['properties']['Name'] =~ /[a-z][0-9]-sector/}
-    puts "SECTOR:", sector
+ 
     if(sector.empty?)
         puts "sector #{sector} not found"
         next
     end
+
     sector_name = sector[0]['properties']['Name'].gsub("-sector", "").gsub("\n", "")
 
     if Sector.find_by(name: sector_name).present? && Sector.find_by(name: sector_name).geojson_string.nil?
@@ -27,20 +33,56 @@ Dir.children(dir_name).each do |file_name|
         next 
     end
 
-
     Sector.create(
         name: sector_name,
         coordinates: sector[0]['geometry']['coordinates'],
         geojson_string: sector.to_s)
 
+
+    # SECOND: create plant entries for each plant in the sector
     data['features'].each do |feature| 
 
         # skip if the feature is a sector
         next if feature['properties']['Name'] =~ /[a-z][0-9]-sector/
 
+        name = feature['properties']['Name'].gsub("\n", "")
+
+        cultivar = Cultivar.where("name ILIKE ?", "%#{name}%").first
+        sector_id = Sector.find_by(name: sector_name).id
+
+        if cultivar.nil?    
+            #don't remember why i'm doing this
+            feature['properties']['Name'] = name
+            failed << plant
+            cultivar_names_to_add << name
+            next 
+        end 
+
+        cultivar_id = cultivar.id
+
+        puts "Creating plant #{name}"
+
+        Plant.create(
+            cultivar_id: cultivar_id,
+            #TODO: should i reverse these coordinates?
+            latitude: feature['geometry']['coordinates'][1],
+            longitude: feature['geometry']['coordinates'][0],
+            sector_id: sector_id
+        )
         
     end
    
+end
+
+
+# append each failed item to the failed file, without overwriting existing entries
+File.open("script/failed.json", "a") do |f|
+    f.write(failed.to_json)
+end
+
+# append each missing cultivar name to the cultivars_to_add file, without overwriting existing entries
+File.open("script/cultivars_to_add.json", "w") do |f|
+    f.write(cultivar_names_to_add.to_json)
 end
 
 
