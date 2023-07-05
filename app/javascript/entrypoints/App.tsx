342d9398f871b16bd439b31b750a332e7e852e51
Matt Seidholz <mseidholz@gmail.com>
2023-07-05 13:56:55 -0400

    add webpack-dev

--- app/javascript/entrypoints/App.tsx
+++ app/javascript/entrypoints/App.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export function App() {
+  useEffect(() => {
+    console.log('IT IS AN EFFECT');
+  }, []);
+
   return (
     <div>
       <h1>I am the App Now</h1>
