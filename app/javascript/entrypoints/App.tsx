ae54d8e9e35af56339780eda4011dd5ced6972a8
Matt Seidholz <mseidholz@gmail.com>
2023-07-05 12:15:03 -0400

    separate App component from application

---
+++ app/javascript/entrypoints/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div>
+      <h1>I am the App Now</h1>
+    </div>
+  );
+}
--- app/javascript/entrypoints/application.tsx
+++ app/javascript/entrypoints/application.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const App = () => <h1>Hello from React!</h1>;
+import { App } from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
